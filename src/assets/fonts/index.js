95aaf82bba3bbe5050f68c7e2db93c7dbb036320
Chris McQuade <vtelbin@hotmail.com>
2025-12-01 17:54:27 +0000

    Update index.js

--- src/assets/fonts/index.js
+++ src/assets/fonts/index.js
@@ -3,23 +3,82 @@
 
 import { createGlobalStyle } from 'styled-components'
 
-/**
- * Add your own custom fonts here.
- * 
- * e.g.
- * 
- * @font-face {
- *   font-family: 'Open_Sans_LtIt';
- *   src: url('${Open_Sans_LtIt}') format('truetype');
- *   font-weight: normal;
- *   font-style: normal;
- * }
- */
 const Fonts = createGlobalStyle`
   @font-face {
-    font-family: 'Arial';
-    font-weight: normal;
+    font-family: VMCircular;
     font-style: normal;
+    font-weight: 300;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/8faf8fae43/vmcircularwhispertt-light.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/7c8fb2045c/vmcircularwhispertt-light.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 400;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/53f93679dc/vmcircularchattt-book.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/b1943ffbd6/vmcircularchattt-book.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 500;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/53f93679dc/vmcircularchattt-book.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/b1943ffbd6/vmcircularchattt-book.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 600;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/c90a340a82/vmcircularshouttt-bold.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/2ba10447e1/vmcircularshouttt-bold.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 700;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/c90a340a82/vmcircularshouttt-bold.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/2ba10447e1/vmcircularshouttt-bold.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 800;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/7140f89ef7/vmcircularyelltt-black.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/e481bf70fb/vmcircularyelltt-black.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: VMCircular;
+    font-style: normal;
+    font-weight: 900;
+    font-stretch: 100% 100%;
+    src: url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/7140f89ef7/vmcircularyelltt-black.woff2) format("woff2"),
+         url(https://storyblok.cdn.vmo2digital.co.uk/f/253875/x/e481bf70fb/vmcircularyelltt-black.ttf) format("truetype");
+    font-display: swap;
+  }
+
+  /* Apply font smoothing like Virgin Media website */
+  body {
+    font-family: "VMCircular", arial, helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `;
 
